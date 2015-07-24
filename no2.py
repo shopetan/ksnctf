5478f23dda7187cc654ea11566b5d4f8e1a5d301
Shohei Kikuchi <s.kikuchi1120@gmail.com>
2015-07-24 13:56:05 +0900

    solve no2

---
+++ no2.py
@@ -0,0 +1,6 @@
+#encode:utf-8
+import codecs
+
+cipher = "EBG KVVV vf n fvzcyr yrggre fhofgvghgvba pvcure gung ercynprf n yrggre jvgu gur yrggre KVVV yrggref nsgre vg va gur nycunorg. EBG KVVV vf na rknzcyr bs gur Pnrfne pvcure, qrirybcrq va napvrag Ebzr. Synt vf SYNTFjmtkOWFNZdjkkNH. Vafreg na haqrefpber vzzrqvngryl nsgre SYNT."
+
+print(codecs.encode(cipher,"rot_13"))
